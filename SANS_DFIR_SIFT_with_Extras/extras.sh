dbaa677e9080fa53232ef944e8a95bba8f033f22
RS Tyler Schroder <11656073+Redorhcs@users.noreply.github.com>
2018-05-16 17:52:47 +0000

    Update extras.sh

--- SANS_DFIR_SIFT_with_Extras/extras.sh
+++ SANS_DFIR_SIFT_with_Extras/extras.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# VOlatility Bot, automated mem forensics
 git clone https://github.com/mkorman90/VolatilityBot.git
 cd VolatilityBot/
 pip install -r requirements.txt
@@ -6,6 +7,12 @@ cd ..
 pip install PyPDF2==1.26.0
 pip install olefile==0.44
 pip install lxml==3.7.3
+# FOrensic TOols, some python scripts
 git clone https://github.com/MonroCoury/Forensic-Tools.git
 cd Forensic-Tools
+# pip install -r requirements.txt
+#
+# DOmain Enumerator
+git clone https://github.com/diogo-fernan/domfind.git
+cd domfind
 pip install -r requirements.txt
