46bc6f7fb5389955fc2124ddd5bd3c955f87bbab
RS Tyler Schroder <11656073+Redorhcs@users.noreply.github.com>
2018-05-17 14:30:01 -0400

    add documenting comments

--- SANS_DFIR_SIFT_with_Extras/sift.sh
+++ SANS_DFIR_SIFT_with_Extras/sift.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-wget https://github.com/sans-dfir/sift-cli/releases/download/v1.6.1/sift-cli-linux
-wget https://github.com/sans-dfir/sift-cli/releases/download/v1.6.1/sift-cli-linux.sha256.asc
-gpg --keyserver pgp.mit.edu --recv-keys 22598A94
-gpg --verify sift-cli-linux.sha256.asc
-sudo mv sift-cli-linux /usr/local/bin/sift
-chmod 755 /usr/local/bin/sift
-sift install
+wget https://github.com/sans-dfir/sift-cli/releases/download/v1.6.1/sift-cli-linux # download sift CLI
+wget https://github.com/sans-dfir/sift-cli/releases/download/v1.6.1/sift-cli-linux.sha256.asc # hash for integ check
+gpg --keyserver pgp.mit.edu --recv-keys 22598A94 # Get GPG key for hash check
+gpg --verify sift-cli-linux.sha256.asc # GPG verification of hash
+sudo mv sift-cli-linux /usr/local/bin/sift # move sift to /bin for command line execution
+chmod 755 /usr/local/bin/sift # set permissions
+sift install # install sift
