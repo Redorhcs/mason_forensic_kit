bdf373fec214b425eb4e9d0f37cfb61296a6d029
1CoolDavid <davidrudo@hotmail.com>
2018-05-19 16:47:08 -0700

    extra package added for ubuntu support

--- Network_Miner/nmi.sh
+++ Network_Miner/nmi.sh
@@ -4,6 +4,7 @@ sudo apt-get install libmono-system-web4.0-cil
 sudo apt-get install libmono-system-net4.0-cil
 sudo apt-get install libmono-system-runtime-serialization4.0-cil
 sudo apt-get install libmono-system-xml-linq4.0-cil
+sudo apt-get install python-mlpy
 wget www.netresec.com/?download=NetworkMiner -O /tmp/nm.zip
 sudo unzip /tmp/nm.zip -d /opt/
 cd /opt/NetworkMiner*
